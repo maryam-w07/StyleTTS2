90927abd1cb6878c42dab336c486ee9c6d767cbe
Maryam <mariamwaqarawan@gmail.com>
2025-08-06 21:06:06 +0500

    Update train_finetune_inf.py

--- train_finetune_inf.py
+++ train_finetune_inf.py
@@ -257,7 +257,7 @@ def main(config_path, inference, audio_path, text):
         with open('/content/phoneme_to_viseme.json', 'r', encoding='utf-8') as f:
             phoneme_to_viseme = json.load(f)
 
-        skip_symbols = set(';:,.!?¡¿—…\"«»“”ǃˈˌːˑʼ˞↓↑→↗↘̩ᵻ1234567890')
+        #skip_symbols = set(';:,.!?¡¿—…\"«»“”ǃˈˌːˑʼ˞↓↑→↗↘̩ᵻ1234567890')
         hop_length = 300
         sample_rate = 24000
         frame_duration_ms = hop_length / sample_rate * 1000  # = 12.5 ms
@@ -272,7 +272,7 @@ def main(config_path, inference, audio_path, text):
 
         # 2. Phonemize the input text
         phoneme_text = phonemize(
-            text, language='en-gb', backend='espeak', strip=False, preserve_punctuation=True, njobs=1,separator=Separator(phone=' ', word=' | ')
+            text, language='en-gb', backend='espeak', strip=False, preserve_punctuation=False, njobs=1,separator=Separator(phone=' ', word=' | ')
         )
         # Optionally remove extra spaces or join if your text_cleaner expects a string
         
